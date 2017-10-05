77d18ead493c07bea9864fcea9e16fc36ac8776b
Albert Lucianto <albertlucianto@gmail.com>
2017-10-06 01:23:14 +0800

    update h line

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="canvas">
       <svg class="hi-container">
-        <circle cx="100" cy="100" r="90" class="line h-curve">
+        <circle cx="100" :cy="hCurveStart" r="90" class="line h-curve">
         </circle>
+        <line x1="190" :y1="hLineRightStart" x2="190" :y2="hLineRightEnd" class="line h-right">
+        </line>
+        <line x1="10" y1="0" x2="10" :y2="hLineLeftEnd" class="line h-left">
+        </line>
       </svg>
   </div>
 </template>
@@ -16,11 +20,17 @@ export default {
     };
   },
   computed: {
-    hCurve() {
-      const progress = Math.max(0, this.frame - 100) * (235.619 / this.totalFrame);
-      return {
-        'stroke-dasharray': `${progress}, 235.619`,
-      };
+    hCurveStart() {
+      return (window.innerHeight / 2) - 75;
+    },
+    hLineRightEnd() {
+      return (window.innerHeight / 2) - 75;
+    },
+    hLineLeftEnd() {
+      return (window.innerHeight / 2) - 75;
+    },
+    hLineRightStart() {
+      return window.innerHeight;
     },
   },
 };
@@ -44,13 +54,17 @@ export default {
       fill: none;
       stroke: #7D4896;
       stroke-linecap: round;
+      transform-origin: 50% 50%;
       &.h-curve {
         animation: h-curve 0.75s forwards ease;
-        animation-delay: 1s;
+        animation-delay: 0.25s;
         transform: rotate(180deg) scaleX(-1);
-        transform-origin: 50% 50%;
         stroke-dasharray: 0, 600;
         opacity: 0;
+      }
+      &.h-right {
+        animation: h-right 0.75s forwards ease;
+        transform: scaleY(-1);
       }
     }
   }
@@ -63,6 +77,15 @@ export default {
   100% {
     stroke-dasharray: 280, 600;
     opacity: 1;
+  }
+}
+
+@keyframes h-right {
+  0% {
+    stroke-dasharray: 1000, 0;
+  }
+  100% {
+    stroke-dasharray: 150, 1000;
   }
 }
 </style>
