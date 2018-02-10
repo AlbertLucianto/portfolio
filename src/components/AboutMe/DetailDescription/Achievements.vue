57bf776ff37a4c8140392712967ecafc9e460ceb
Albert Lucianto <albertlucianto@gmail.com>
2018-02-10 18:05:20 +0800

    add more about me page

---
+++ src/components/AboutMe/DetailDescription/Achievements.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="achievements-container">
+    <div class="h1__container">
+      <h1>Personal Achievements</h1>
+    </div>
+    <div>
+      <div v-for="(achievement, idx) in achievements" :key="idx" class="achievement">
+        <div class="titleAndTime">
+          <div class="achievement__title">{{ achievement.title }}</div>
+          <div class="achievement__time">{{ achievement.time }}</div>
+        </div>
+        <div class="achievement__issuer">{{ achievement.issuer }}</div>
+        <div class="separator"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      achievements: [
+        {
+          title: 'Dean\'s List (top 5% cohort)',
+          issuer: 'Nanyang Technological University, School of Computer Science and Engineering',
+          time: 'AY 2015/2016',
+        },
+        {
+          title: 'Silver Medal',
+          issuer: 'Indonesian National Science Olympiad (Mathematics)',
+          time: '2014',
+        },
+        {
+          title: 'Bronze Medal',
+          issuer: 'Indonesian National Science Olympiad (Mathematics)',
+          time: '2013 & 2012',
+        },
+        {
+          title: 'Bronze Medal',
+          issuer: 'International Mathematics Competition (IMC)',
+          time: '2011',
+        },
+        {
+          title: '4th Runner Up',
+          issuer: 'Global Art International Competition in Singapore',
+          time: '2009',
+        },
+        {
+          title: 'Best of the Best',
+          issuer: 'Global Art National Competition Indonesia',
+          time: '2009',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/colors.scss';
+
+.achievements-container {
+  box-sizing: border-box;
+  margin-top: 105px;
+  padding: 25px 45px;
+  height: calc(100% - 90px);
+  background: $white;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: $orange;
+    border-radius: 5px;
+  }
+  .h1__container {
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: $white;
+    width: 100%;
+    padding-top: 15px;
+    z-index: 10;
+    box-shadow: 0 -5px 45px -15px $grey;
+    h1 {
+      font-size: 32px;
+    }
+  }
+  .achievement {
+    margin: 30px 0;
+    .titleAndTime {
+      display: flex;
+      justify-content: center;
+      .achievement__title {
+        font-weight: 600;
+        color: $purple;
+      }
+      .achievement__time {
+        opacity: .6;
+        margin-left: 10px;
+      }
+    }
+    .achievement__issuer {
+      font-size: 18px;
+    }
+    .separator {
+      height: 8px;
+      width: 25px;
+      margin: auto;
+      margin-top: 20px;
+      background: $grey;
+      border-radius: 10px;
+      opacity: .7;
+    }
+  }
+}
+</style>
---
+++ src/components/AboutMe/DetailDescription/Portfolios.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="portfolio__container">
+    <img src="../../../assets/behance-logo.png" class="behanceLogo"/>
+    <a href="https://behance.net/albertlucianto" class="profile__link">
+      <div class="profile">
+        <div class="username">{{ profile.username }}</div>
+        <div class="separator"/>
+        <div class="profile__item">
+          <div class="label">PROJECT VIEWS</div>
+          <div class="value">{{ profile.projectViews }}</div>
+        </div>
+        <div class="profile__item">
+          <div class="label">APPRECIATION</div>
+          <div class="value">{{ profile.appreciation }}</div>
+        </div>
+        <div class="profile__item">
+          <div class="label">FOLLOWERS</div>
+          <div class="value">{{ profile.followers }}</div>
+        </div>
+        <div class="profile__item">
+          <div class="label">FOLLOWING</div>
+          <div class="value">{{ profile.following }}</div>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+// const BEHANCE_CLIENT_ID = 'r26abOGUOCAJUSohAr2c3b4vgYY7Dz5E';
+// const URL = (username, clientId) => `https://api.behance.net/v2/users/${username}?client_id=${clientId}`;
+
+export default {
+  data() {
+    return {
+      profile: {
+        /**
+         * Stupid hardcode, Behance API requires
+         * to be fetched from server to avoid CORS
+         * */
+        username: 'albertlucianto',
+        projectViews: 13245,
+        appreciation: 1622,
+        followers: 550,
+        following: 138,
+      },
+    };
+  },
+  mounted() {
+    // fetch(URL(this.username, BEHANCE_CLIENT_ID))
+    // .then(data => console.log(data))
+    // .then((profile) => { this.profile = profile; });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/colors.scss';
+
+.portfolio__container {
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  padding: 60px 30px;
+  .behanceLogo {
+    width: 200px;
+    margin-top: 15px;
+  }
+  .profile__link {
+    color: $black;
+    text-decoration: none;
+  }
+  .profile {
+    margin: auto;
+    width: 50%;
+    margin-top: 40px;
+    padding: 25px 40px;
+    padding-top: 20px;
+    border-radius: 10px;
+    box-shadow: -10px 10px 50px -15px rgb(0, 122, 255);
+    background: $white;
+    .username {
+      font-weight: 600;
+      font-size: 32px;
+    }
+    .separator {
+      border-bottom: 1px solid $grey;
+      margin-top: 10px;
+      margin-bottom: 15px;
+      width: 50%;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    .profile__item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 3px 0;
+      .label {
+        font-weight: 600;
+        font-size: 14px;
+      }
+      .value {
+        opacity: .8;
+      }
+    }
+  }
+}
+</style>
